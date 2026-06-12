41df53c84605bd6d183e54969cc4898fe8d78d19
rwmerrigan <76187538+rwmerrigan@users.noreply.github.com>
2026-06-12 16:50:08 -0700

    json

--- PythonFoundationsCourse/weather_api.py
+++ PythonFoundationsCourse/weather_api.py
@@ -2,6 +2,7 @@
 # call - http://api.weatherapi.com/v1/current.json?key=b062659974cf443ba7d171322262705&q=Irvine&aqi=no
 
 import requests
+import json
 
 city = 'Irvine'
 url = 'http://api.weatherapi.com/v1/current.json?key=b062659974cf443ba7d171322262705&q=' + city + '&aqi=no'
@@ -10,5 +11,8 @@ weather_json = response.json()
 
 temp = weather_json.get('current').get('temp_f')
 description = weather_json.get('current').get('condition').get('text')
+print("\nData printed in a pretty format with JSON module --------")
+print(json.dumps(weather_json, indent=4))
+print("Data printed in a pretty format with JSON module --------")
 
 print("Today's weather in", city, "is", description, 'and', temp, 'degrees')
